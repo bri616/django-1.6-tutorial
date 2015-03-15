9dcb20354c0f73fac637c90eb7e8aff0be9d1ce4
Bri Dotson <bridotson@gmail.com>
2015-03-15 13:32:42 -0700

    add polladmin

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from polls.models import Poll
 
 # Register your models here.
+
+class PollAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+admin.site.register(Poll, PollAdmin)
+
